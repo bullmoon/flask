b23c734324c9da03bc4567711075d6092394f592
Scrooge McDuck <vlad.vysotskiy@gmail.com>
2021-06-14 00:56:15 +0300

    Used Jinja Changed hello.py

--- hello.py
+++ hello.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='About Flask')
 
 @app.route("/about")
 def about():
-    return "<h1>About Flask...</h1>"
+    return render_template('about.html', title='About site'))
 
 if __name__ == "__main__":
     app.run(debug=True)
