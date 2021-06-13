e30695d1edea6167d69956a2e1a8044d0acdc2f6
Scrooge McDuck <vlad.vysotskiy@gmail.com>
2021-06-14 00:44:08 +0300

    Added templates Changed hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hello, World!"
-# if __name__ == '__main__':
-#    app.run(port=5000, debug=True)
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return "<h1>About Flask...</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
